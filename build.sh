e6c05b0bb3edd8aec8571699b31b7c3af8dfab70
Eduardo A. Diez <eduadiez@gmail.com>
2022-09-06 22:42:11 +0200

    fix permisions

--- build.sh
+++ build.sh
@@ -6,11 +6,13 @@ git clone ${MASTER_REPO} master_repo
 
 for SRC in $(find master_repo -name "*-build*")
 do
-    echo $SRC
     DST=${SRC/master_repo/.}
     DST=${DST/-build}
-    echo $DST
     if [ ! -f $DST ];then
         envsubst '$_BUILD_NETWORK $_BUILD_PACKAGE_NAME $_BUILD_UPSTREAM_VERSION $_BUILD_P2P_PORT $_BUILD_BITTORRENT_PORT $_BUILD_CONSENSUS_PREFIX' < $SRC > $DST
     fi
+    if [ "${file: -4}" == ".txt" ]; then
+        chmod +x $DST
+    fi
 done
+
